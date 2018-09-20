9bf84b6ec5c370f229643eb7c6ee6f59451a26aa
Daniel <35694200+danielTobon43@users.noreply.github.com>
2018-09-19 21:09:21 -0500

    Correction in ply reader

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,7 @@
 #include <string>
 
 void printUsage (const char* progName){
-  std::cout << "\nUsage: " << progName << " <file.ply> or <file.txt> or <file.xyz>"  << std::endl <<
-               "[q] to exit" << std::endl;
+  std::cout << "\nUsage: " << progName << " <file.ply> -o <output dir>"  << std::endl;
 }
 
 using namespace std;
@@ -36,13 +35,14 @@ int main(int argc, char **argv){
   bool file_is_ply = false;
   bool file_is_txt = false;
   bool file_is_xyz = false;
-  pcl::console::TicToc tt;
-  pcl::console::print_highlight ("Loading ");
 
-  if(argc < 2 or argc > 2){
+  if(argc < 4 or argc > 4){
       printUsage (argv[0]);
       return -1;
   }
+
+  pcl::console::TicToc tt;
+  pcl::console::print_highlight ("Loading ");
 
   filenames = pcl::console::parse_file_extension_argument(argc, argv, ".ply");
   if(filenames.size()<=0){
@@ -71,26 +71,39 @@ int main(int argc, char **argv){
       return -1;
   }
 
-  if(file_is_pcd){
- 
+  std::string output_dir;
+  if(pcl::console::parse_argument(argc, argv, "-o", output_dir) == -1){
+      PCL_ERROR ("Need an output directory! Please use <input cloud> -o <output dir>\n");
+      return(-1);
+  }
+
+  if(file_is_pcd){ 
       pcl::console::print_info("\nFound pcd file.\n");
       return -1;
     }else if(file_is_ply){
-      if(pcl::io::loadPolygonFile(argv[filenames[0]], cl) < 0){
-          std::cout << "Error loading point cloud " << argv[filenames[0]] << "\n";
-          printUsage (argv[0]);
-          return -1;
-        }else{
-          pcl::io::loadPLYFile(argv[filenames[0]], *cloud);
-          //pcl::fromPCLPointCloud2(cl.cloud, *cloud);
-          pcl::console::print_info("\nFound ply file.\n");
-          pcl::console::print_info ("[done, ");
-          pcl::console::print_value ("%g", tt.toc ());
-          pcl::console::print_info (" ms : ");
-          pcl::console::print_value ("%d", cloud->size ());
-          pcl::console::print_info (" points]\n");
-          
-        }
+        pcl::io::loadPolygonFile(argv[filenames[0]],cl);
+        pcl::fromPCLPointCloud2(cl.cloud, *cloud);
+        if(cloud->points.size()<=0){
+            pcl::console::print_warn("\nloadPolygonFile could not read the cloud, attempting to loadPLYFile...\n");
+            pcl::io::loadPLYFile(argv[filenames[0]], *cloud);
+            if(cloud->points.size()<=0){
+                pcl::console::print_warn("\nloadPLYFile could not read the cloud, attempting to PLYReader...\n");
+                pcl::PLYReader plyRead;
+                plyRead.read(argv[filenames[0]],*cloud);
+                if(cloud->points.size()<=0){
+                    pcl::console::print_error("\nError. ply file is not compatible.\n");
+                    return -1;
+                }
+            }
+         }
+
+        pcl::console::print_info("\nFound ply file.\n");
+        pcl::console::print_info ("[done, ");
+        pcl::console::print_value ("%g", tt.toc ());
+        pcl::console::print_info (" ms : ");
+        pcl::console::print_value ("%d", cloud->size ());
+        pcl::console::print_info (" points]\n");
+
     }else if(file_is_txt or file_is_xyz){
       std::ifstream file(argv[filenames[0]]);
       if(!file.is_open()){
@@ -112,15 +125,20 @@ int main(int argc, char **argv){
       pcl::console::print_info (" ms : ");
       pcl::console::print_value ("%d", cloud->size ());
       pcl::console::print_info (" points]\n");
-
-    }
+  }
 
   cloud->width = (int) cloud->points.size ();
   cloud->height = 1;
   cloud->is_dense = true;  
 
-  pcl::console::print_info ("saved point cloud.\n");
-  pcl::io::savePCDFileBinary("prueba.pcd",*cloud);
+  output_dir += "/cloudConverted.pcd";
+
+  std::string sav = "saved point cloud in:";
+  sav += output_dir;
+
+  pcl::console::print_info(sav.c_str());
+  std::cout << std::endl;
+  pcl::io::savePCDFileBinary(output_dir.c_str(),*cloud);
 
   return 0;
 }
